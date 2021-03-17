f08171af7f18b93dbea8755e97244e6789fc1b95
skiblue3 <myzticblue126@gmail.com>
2021-03-18 00:23:51 +0530

    various input for grid size

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-const container = document.createElement('div');
-container.setAttribute('id', 'container');
-
-for (let i = 0; i < 16*16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
-}
-
-document.querySelector('body').appendChild(container);
-
 // function to get random value out of 255
 function random255() {
     return Math.floor(Math.random() * 255);
 }
 
-// check for hovering event
-const divs = document.querySelectorAll('.grid');
+function makeGrid(size) {
+    // deletes previous grid
+    document.querySelectorAll('.grid').forEach((div) => {div.remove()});
 
-divs.forEach((div) => {
-    div.addEventListener('mouseover', function (e) {
-        div.style.backgroundColor = `rgb(${random255()},${random255()},${random255()})`;
+    // creates new grid using the given size
+    for (let i = 0; i < size*size; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+    }
+    container.style.gridTemplateColumns = `repeat(${size}, ${420/size}px)`;
+    container.style.gridTemplateRows = `repeat(${size}, ${420/size}px)`;
+
+    // check for hovering event
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', function (e) {
+            div.style.backgroundColor = `rgb(${random255()},${random255()},${random255()})`;
+        });
     });
+}    
+
+
+// create a container to store all divs
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+
+let size = 16;
+makeGrid(16); // default grid of 16x16
+
+// when change size button is clicked
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+    while (true) {
+        size = prompt("enter size: ", "16");
+        size = parseInt(size);
+        if (size > 0 && size < 100) 
+            break;
+        else 
+            alert("please enter in the range of 1-99!");
+    }
+    makeGrid(size);
 });
+
+document.querySelector('body').appendChild(container);
+
